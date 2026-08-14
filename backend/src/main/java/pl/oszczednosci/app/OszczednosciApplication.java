fdd3b3d04a23cd050c2e4873bcaa7ea0ddf91f2b
KubaUrbanek <77888801+KubaUrbanek@users.noreply.github.com>
2026-08-14 14:25:32 +0200

    Add config tree master password mapping

--- backend/src/main/java/pl/oszczednosci/app/OszczednosciApplication.java
+++ backend/src/main/java/pl/oszczednosci/app/OszczednosciApplication.java
@@ -2,8 +2,10 @@ package pl.oszczednosci.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class OszczednosciApplication {
 
     public static void main(String[] args) {
